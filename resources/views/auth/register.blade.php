cfbcbe2854e27804f0ca020309450807bc31adbd
K_ozawa <ozawa19890413@gmail.com>
2020-05-24 21:05:46 +0900

    [modify] 新規登録ページ

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -30,11 +30,9 @@
                             <label for="birthday" class="col-md-4 col-form-label text-md-right">生年月日</label>
 
                             <div class="col-md-6">
-                                <input id="bday_year" type="number" class="form-control @error('bday_year') is-invalid @enderror" name="bday_year" value="{{ old('bday_year') }}" required autocomplete="bday" placeholder="1980">年
-
-                                <input id="bday_month" type="number" class="form-control @error('bday_month') is-invalid @enderror" name="bday_month" value="{{ old('bday_month') }}" required autocomplete="bday" placeholder="2">月
-
-                                <input id="bday_day" type="number" class="form-control @error('bday_day') is-invalid @enderror" name="bday_day" value="{{ old('bday_day') }}" required autocomplete="bday" placeholder="14">日
+                                <select id="bday_year" class="form-control"><option value="0">----</option></select>年
+                                <select id="bday_month" class="form-control"><option value="0">--</option></select>月
+                                <select id="bday_day" class="form-control"><option value="0">--</option></select>日​
 
                                 @error('birthday')
                                     <span class="invalid-feedback" role="alert">
@@ -50,7 +48,7 @@
                             <label for="comment" class="col-md-4 col-form-label text-md-right">コメント</label>
 
                             <div class="col-md-6">
-                                <textarea id="comment" class="form-control @error('comment') is-invalid @enderror" name="comment" placeholder="ふんどし王子です。余市第1リフトで仕事してます。プログラマーやったり自衛官やったり家事代行やったりしてきました。南樽市場安くて幸せ。">{{ old('comment') }}</textarea>
+                                <textarea id="comment" class="row-5 form-control @error('comment') is-invalid @enderror" name="comment" placeholder="ふんどし王子です。余市第1リフトで仕事してます。プログラマーやったり自衛官やったり家事代行やったりしてきました。南樽市場安くて幸せ。">{{ old('comment') }}</textarea>
 
                                 @error('comment')
                                     <span class="invalid-feedback" role="alert">
