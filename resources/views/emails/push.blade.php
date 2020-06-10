5d5f5fd47c0268ff52cb9d1276adc232666d010a
牛島一樹 <kazuki@MacBook-Kazuki-12-inch-Early-2015.local>
2020-06-10 19:44:39 +0900

    misc. modif. of mail template for push

--- resources/views/emails/push.blade.php
+++ resources/views/emails/push.blade.php
@@ -1,19 +1,19 @@
 @component('mail::message')
-  @if($post_index)
+@if($post_index)
 # {{ $board->name }}に書き込みがありました
-{{ $post_index }}
-  @elseif($user_info)
+{{ $post_index }}  
+@elseif($user_info)
 # {{ $board->name }}に新しいユーザーが参加しました
-{{ $user_info }}
-  @elseif($letter)
+{{ $user_info }}  
+@elseif($letter)
 # {{ $letter->from_user->profile }}から新しいメッセージが届きました
-{{ Str::limit($letter->content, 40, '...') }}　　
-  @endif
+{{ Str::limit($letter->content, 40, '...') }}  
+@endif
 
-  @if( $board )
-[{{ $board->name }}を表示する]({{ $board->url }})　　
-  @else
-[受信ボックスを表示する]({{ route('letters.inbox') }})
-  @endif
-@endcomponent
+@if( $board )
+[{{ $board->name }}を表示する]({{ $board->url }})  
+@else
+[受信ボックスを表示する]({{ route('letters.inbox') }})  
+@endif
 [通知設定を変更する]({{ route('home.settings') }})
+@endcomponent
